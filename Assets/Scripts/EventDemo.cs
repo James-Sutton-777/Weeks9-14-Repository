e1fa650ea3d667182469584956498cd30d8009bd
James Sutton <suttjame@shernet.sheridancollege.ca>
2025-03-13 14:50:47 -0400

    Created timer envent trigger

--- Assets/Scripts/EventDemo.cs
+++ Assets/Scripts/EventDemo.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EventDemo : MonoBehaviour
 {
     public RectTransform banana;
+
+    public UnityEvent OnTimerHasFinished;
+
+    public float timerLength = 3;
+    public float timer;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timerLength)
+        {
+            timer = 0;
+            OnTimerHasFinished.Invoke();
+        }
+    }
+
     public void MouseJustEnteredImage()
     {
         Debug.Log("Mouse just entered me!");
