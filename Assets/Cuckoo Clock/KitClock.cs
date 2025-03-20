aefa6cb46fbaa4d0e5ef3a9af13a32834fc24eb4
James Sutton <suttjame@shernet.sheridancollege.ca>
2025-03-20 13:53:21 -0400

    Complete clock movement -Nested Coroutine -Stop Button

--- Assets/Cuckoo Clock/KitClock.cs
+++ Assets/Cuckoo Clock/KitClock.cs
@@ -9,26 +9,47 @@ public class KitClock : MonoBehaviour
     public Transform minuteHand;
     public float timeAnHourTakes = 5;
 
+    Coroutine clockIsRunning;
+    Coroutine handsMovingOneHour;
+
     public float t;
     public int hour = 0;
 
     public UnityEvent OnTheHour;
-
-    void Start()
+    private void Start()
     {
-        StartCoroutine(MoveClockHandsOneHour());
+        clockIsRunning = StartCoroutine(ClockCompleteLocamotion());
+    }
+
+    IEnumerator ClockCompleteLocamotion()
+    {
+        //Using a single line of code multiple times indicates potential to be turned into a loop
+        //Using a the true == true allows the loop to run perpetually, however the yield allows the game to retain control of what code is running
+        while(true)
+        {
+            yield return handsMovingOneHour = StartCoroutine(MoveClockHandsOneHour());
+        }
     }
     //coroutine moves hour and minute hand the equivalent of 1 hour of movement
     IEnumerator MoveClockHandsOneHour()
     {
+        t = 0;
         while(t < timeAnHourTakes)
         {
             t += Time.deltaTime;
 
             minuteHand.Rotate(0, 0, -(360 / timeAnHourTakes) * Time.deltaTime);
             hourHand.Rotate(0, 0, -(30 / timeAnHourTakes) * Time.deltaTime);
+
+            //yield instruct it to return to line 33 next frame which being the end of the loop causes the program to check to the loop.
             yield return null;
         }
         OnTheHour.Invoke();
+    }
+
+    public void StopClock()
+    {
+        StopCoroutine(clockIsRunning);
+        StopCoroutine(handsMovingOneHour);
     }
 }
