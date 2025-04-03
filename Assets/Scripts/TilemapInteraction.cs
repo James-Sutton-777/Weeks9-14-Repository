07d74ea84770e7e78d41bf3248af6a8ef26dc306
James Sutton <suttjame@shernet.sheridancollege.ca>
2025-04-03 14:18:31 -0400

    Added Script to Retrieve Tile Under Cursor

---
+++ Assets/Scripts/TilemapInteraction.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapInteraction : MonoBehaviour
+{
+
+    public Tilemap tilemap;
+
+    public Tile grass;
+    public Tile stone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3Int gridPos = tilemap.WorldToCell(mousePos);
+
+            tilemap.GetTile(gridPos);
+        }
+    }
+}
