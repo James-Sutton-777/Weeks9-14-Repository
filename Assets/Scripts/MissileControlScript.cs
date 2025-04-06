0aa75d7c3e81ed2b4857a5f426bf6cc727bc2748
James Sutton <suttjame@shernet.sheridancollege.ca>
2025-04-06 17:58:24 -0400

    Missile Now Functions As Intended

--- Assets/Scripts/MissileControlScript.cs
+++ Assets/Scripts/MissileControlScript.cs
@@ -8,26 +8,33 @@ public class MissileControlScript : MonoBehaviour
 
     public PlayerControlScript Target;
 
+    public bool targetValidated = true;
+
     public float predictionTime;
-    public float predictionModifier;
+    public float staticProjection;
     public float speed;
-    public float time;
-    public float projection;
-    public float launchDistance;
-    public float currentDistance;
+    public float projectionDistance;
     public float distanceToTarget;
+    public float thresholdStageTwo;
+    public float missileTurnSpeed;
+    public float seekerAngleThreshold;
 
     public AnimationCurve curve;
 
-    Vector3 missileDirection;
+    Vector3 targetProjection;
     Vector3 missileVelocity;
     Vector3 seekerDirection;
     Vector3 targetDirection;
+    Vector3 seekerTracking;
+
+    Vector3 targetPosition;
+    Vector3 targetVelocity;
+
+    Quaternion targetRotation;
     // Start is called before the first frame update
     void Start()
     {
         targetDirection = Target.transform.position - transform.position;
-        launchDistance = targetDirection.magnitude;
     }
 
     // Update is called once per frame
@@ -36,21 +43,47 @@ public class MissileControlScript : MonoBehaviour
         //seekerDirection = Target.transform.position - transform.position;
         //transform.up = seekerDirection.normalized;
 
-        time += 1 * Time.deltaTime;
+        TargetValidation();
+        MissileSeeker();
 
-        targetDirection = Target.transform.position - transform.position;
-        currentDistance = targetDirection.magnitude;
+        targetDirection = targetProjection - transform.position;
+        distanceToTarget = targetDirection.magnitude;
 
 
-        distanceToTarget = launchDistance - currentDistance;
-        predictionModifier = Mathf.Lerp(0, 1, distanceToTarget);
+        if( distanceToTarget > thresholdStageTwo)
+        {
+            projectionDistance = staticProjection;
+        }
+        else
+        {
+            projectionDistance = curve.Evaluate(1 - distanceToTarget / thresholdStageTwo) * staticProjection;
+        }
         
-        projection = curve.Evaluate(predictionModifier);
-        
 
-        missileDirection = Target.transform.position + (Target.velocityOfPlayer);
-        transform.up = missileDirection.normalized;
+        targetProjection = targetPosition + (targetVelocity * projectionDistance);
+        if(targetValidated == true)
+        {
+            targetRotation = Quaternion.LookRotation(Vector3.forward, (targetProjection - transform.position).normalized);
+        }
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, missileTurnSpeed * Time.deltaTime);
         transform.position += transform.up * speed * Time.deltaTime;
         
+    }
+
+    void MissileSeeker()
+    {
+        seekerTracking = targetDirection.normalized;
+
+        if (Vector3.Angle(seekerTracking, targetPosition) > seekerAngleThreshold)
+        {
+            Debug.Log("targetlost");
+            targetValidated = false;
+        }
+    }
+
+    void TargetValidation()
+    {
+        targetPosition = Target.transform.position;
+        targetVelocity = Target.velocityOfPlayer;
     }
 }
