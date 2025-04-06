ccf9ef6980f10fb8050dde1415e51ff2b5b0488b
James Sutton <suttjame@shernet.sheridancollege.ca>
2025-04-06 16:32:21 -0400

    New Missile Code added launcher

--- Assets/Scripts/MissileControlScript.cs
+++ Assets/Scripts/MissileControlScript.cs
@@ -11,16 +11,23 @@ public class MissileControlScript : MonoBehaviour
     public float predictionTime;
     public float predictionModifier;
     public float speed;
+    public float time;
+    public float projection;
+    public float launchDistance;
+    public float currentDistance;
+    public float distanceToTarget;
 
-    public AnimationCurve distanceToTarget;
+    public AnimationCurve curve;
 
     Vector3 missileDirection;
     Vector3 missileVelocity;
     Vector3 seekerDirection;
+    Vector3 targetDirection;
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetDirection = Target.transform.position - transform.position;
+        launchDistance = targetDirection.magnitude;
     }
 
     // Update is called once per frame
@@ -28,9 +35,20 @@ public class MissileControlScript : MonoBehaviour
     {
         //seekerDirection = Target.transform.position - transform.position;
         //transform.up = seekerDirection.normalized;
-        //predictionModifier = 
 
-        missileDirection = Target.transform.position + Target.velocityOfPlayer * predictionModifier;
+        time += 1 * Time.deltaTime;
+
+        targetDirection = Target.transform.position - transform.position;
+        currentDistance = targetDirection.magnitude;
+
+
+        distanceToTarget = launchDistance - currentDistance;
+        predictionModifier = Mathf.Lerp(0, 1, distanceToTarget);
+        
+        projection = curve.Evaluate(predictionModifier);
+        
+
+        missileDirection = Target.transform.position + (Target.velocityOfPlayer);
         transform.up = missileDirection.normalized;
         transform.position += transform.up * speed * Time.deltaTime;
         
