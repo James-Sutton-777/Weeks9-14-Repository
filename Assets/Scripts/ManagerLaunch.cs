b60c1c0ef5415cd155b3c55b38f64a8eb4dc2f57
James Sutton <suttjame@shernet.sheridancollege.ca>
2025-04-08 11:55:13 -0400

    Added Missile Spawning Code to Manager Script

--- Assets/Scripts/ManagerLaunch.cs
+++ Assets/Scripts/ManagerLaunch.cs
@@ -7,14 +7,22 @@ public class ManagerLaunch : MonoBehaviour
 {
 
     public PlayerControlScript Target;
+    public GameObject Player;
 
-    public GameObject Missiles;
-    public GameObject Indicators;
+    public List<GameObject> missiles;
+    public List<GameObject> indicators;
+
+    public GameObject missilePrefab;
+    public GameObject indicatorPrefab;
+
+    public float maxMissileCount;
+    public float timer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        missiles = new List<GameObject>();
+        indicators = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -22,5 +30,17 @@ public class ManagerLaunch : MonoBehaviour
     {
         Vector3 targetDirection = (Target.transform.position - transform.position).normalized;
         transform.up = targetDirection;
+    }
+
+    void MissileAndIndicatorSpawner()
+    {
+        timer += 1 * Time.deltaTime;
+        if (timer > 5)
+        {
+            GameObject newMissile = Instantiate(missilePrefab, transform.position, transform.rotation);
+            GameObject newIndicator = Instantiate(indicatorPrefab, Player.transform.position, Quaternion.identity);
+
+            timer = 0;
+        }
     }
 }
