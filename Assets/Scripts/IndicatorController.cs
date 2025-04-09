f7b0197b061e6869940651aa9760c73d5e80a954
James Sutton <suttjame@shernet.sheridancollege.ca>
2025-04-09 15:55:24 -0400

    Commented All Code

--- Assets/Scripts/IndicatorController.cs
+++ Assets/Scripts/IndicatorController.cs
@@ -5,29 +5,31 @@ using static UnityEngine.GraphicsBuffer;
 
 public class IndicatorController : MonoBehaviour
 {
+    //create object refrence to player and assigned missile
     public GameObject threat;
     public GameObject player;
-
+    //assigns distance at which indicator is offset from player
     public float distanceFromPlayer;
-
+    //vector containing missile direction from player
     Vector3 threatDirection;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        //calculate missile direction
         threatDirection = threat.transform.position - player.transform.position;
+        //place indicator at distance between player and missile
         transform.position = player.transform.position + threatDirection.normalized * distanceFromPlayer;
+        //make indicator point towards missile
         transform.up = threatDirection.normalized;
     }
 
+    //function to destroy indicator when missile despawns
     public void MissileDead()
     {
+        //destroys indicator in 3 seconds
         Destroy(gameObject,3);
+        //deactivates indicator spriterender
         SpriteRenderer indicator = GetComponent<SpriteRenderer>();
         indicator.enabled = false;
     }
--- Assets/Scripts/PlayerControlScript.cs
+++ Assets/Scripts/PlayerControlScript.cs
@@ -31,6 +31,7 @@ public class PlayerControlScript : MonoBehaviour
     public float dragCoefficient;
 
     //Vectors to control movement
+    //Velocity is public so that missile can calculate trajectory
     public Vector3 velocityOfPlayer;
     Vector3 directionOfPlayer;
     Vector3 dragOnPlayer;
@@ -45,26 +46,33 @@ public class PlayerControlScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //call function to control player sprite
         AircraftControls();
     }
 
     void AircraftControls()
     {
+        //Determines amount of thrust being generated
         engineOutput = Mathf.Clamp(engineOutput, 0, 1);
         thrust = enginePower * engineOutput;
 
+        //Part of old system used to modify area based on angle difference between velocity and direction player is facing
         directionOfPlayer = transform.up;
 
         //turning calculations
         transform.Rotate(Vector3.forward * ((turnSpeed * turnSpeedRatio) * Input.GetAxisRaw("Horizontal")) * Time.deltaTime);
 
+        //calculate drag created by player to impact top speed and acceleration
         dragOnPlayer = -1 * (dragCoefficient * area * (speed * speed) * velocityOfPlayer.normalized);
 
+        //callculate velocity than add it to position to update player position
         velocityOfPlayer += (dragOnPlayer + (transform.up * thrust)) * Time.deltaTime;
         transform.position += velocityOfPlayer * Time.deltaTime;
 
+        //use magnitude of velocity to generate speed variable for drag calculation
         speed = velocityOfPlayer.magnitude;
 
+        //if statements dedecting keyboard inputs coresponding with engine output generating a percentage of thrust
         if (Input.GetAxisRaw("Vertical") == 1)
         {
             engineOutput += outputForce * Time.deltaTime;
@@ -76,10 +84,13 @@ public class PlayerControlScript : MonoBehaviour
         }
     }
 
+    //function called by missile to initiate the death animation
     public void HitByMissile()
     {
+        //player sprite is turn off
         SpriteRenderer PlayerModel = GetComponent<SpriteRenderer>();
         PlayerModel.enabled = false;
+        //explosion is spawn on top of player position at time of impact;
         GameObject explosion = Instantiate(DeathSphere, transform.position, Quaternion.identity);
     }
 }
